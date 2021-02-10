c37b2c91f8d3826ddbae93428329e153ff00493f
Chin-Chau Low <cc@zipify.net>
2021-02-10 15:53:59 +0800

    incrementByDelta() test

--- src/main/java/com/example/rsmdemo/CounterStore.java
+++ src/main/java/com/example/rsmdemo/CounterStore.java
@@ -1,15 +1,26 @@
 package com.example.rsmdemo;
 
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Sinks;
 
 public class CounterStore {
 	private Long state;
+	private Sinks.Many<Long> valueSink = Sinks.many()
+	                                          .multicast()
+	                                          .onBackpressureBuffer();
 
 	public CounterStore(long initial) {
 		this.state = initial;
+		this.valueSink.tryEmitNext(this.state);
 	}
 
 	public Flux<Long> value$() {
-		return Flux.just(this.state);
+		return this.valueSink.asFlux();
+	}
+
+	public void increment(long delta) {
+		this.state += delta;
+		// emit new state into value$
+		this.valueSink.tryEmitNext(this.state);
 	}
 }
--- src/test/java/com/example/rsmdemo/CounterStoreTests.java
+++ src/test/java/com/example/rsmdemo/CounterStoreTests.java
@@ -1,22 +1,38 @@
 package com.example.rsmdemo;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import reactor.core.publisher.Flux;
 import reactor.test.StepVerifier;
 
 import java.util.Random;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 class CounterStoreTests {
+		long initial;
+		CounterStore counterStore;
+		Flux<Long> subject;
+
+	@BeforeEach void setUp() {
+		initial = randomLong();
+		counterStore = new CounterStore(initial);
+		subject = counterStore.value$();
+	}
+
+	private long randomLong() {
+		return new Random().nextLong();
+	}
 
 	@Test void initialValue() {
-		long initial = new Random().nextLong();
-		CounterStore counterStore = new CounterStore(initial);
-		Flux<Long> subject = counterStore.value$();
-
-		StepVerifier.create(subject)
+		StepVerifier.create(subject.take(1))
 		            .expectNext(initial)
+		            .verifyComplete();
+	}
+
+	@Test void incrementByDelta() {
+		long delta = randomLong();
+		StepVerifier.create(subject.take(2))
+		            .then(() -> counterStore.increment(delta))
+		            .expectNext(initial, initial + delta)
 		            .verifyComplete();
 	}
 }
