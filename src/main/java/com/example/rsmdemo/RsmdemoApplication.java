01f20131eaf3203c2afb39f536eaa37985abfce2
Chin-Chau Low <cc@zipify.net>
2021-02-10 12:51:10 +0800

    initial commit

---
+++ src/main/java/com/example/rsmdemo/RsmdemoApplication.java
@@ -0,0 +1,12 @@
+package com.example.rsmdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication public class RsmdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RsmdemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/rsmdemo/RsmdemoApplicationTests.java
@@ -0,0 +1,11 @@
+package com.example.rsmdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest class RsmdemoApplicationTests {
+
+	@Test void contextLoads() {
+	}
+
+}
