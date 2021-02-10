b5c7066c6e9283b93029dd2af52614a374697fa0
Chin-Chau Low <cc@zipify.net>
2021-02-10 16:10:55 +0800

    AtomicLong

--- src/main/java/com/example/rsmdemo/CounterStore.java
+++ src/main/java/com/example/rsmdemo/CounterStore.java
@@ -3,15 +3,17 @@ package com.example.rsmdemo;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Sinks;
 
+import java.util.concurrent.atomic.AtomicLong;
+
 public class CounterStore {
-	private Long state;
+	private AtomicLong state;
 	private Sinks.Many<Long> valueSink = Sinks.many()
 	                                          .multicast()
 	                                          .onBackpressureBuffer();
 
 	public CounterStore(long initial) {
-		this.state = initial;
-		this.valueSink.tryEmitNext(this.state);
+		this.state = new AtomicLong(initial);
+		this.valueSink.tryEmitNext(this.state.get());
 	}
 
 	public Flux<Long> value$() {
@@ -19,13 +21,11 @@ public class CounterStore {
 	}
 
 	public void increment(long delta) {
-		this.state += delta;
 		// emit new state into value$
-		this.valueSink.tryEmitNext(this.state);
+		this.valueSink.tryEmitNext(this.state.addAndGet(delta));
 	}
 
 	public void decrement(long delta) {
-		this.state -= delta;
-		this.valueSink.tryEmitNext(this.state);
+		this.valueSink.tryEmitNext(this.state.addAndGet(-delta));
 	}
 }
